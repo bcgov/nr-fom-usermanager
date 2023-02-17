1dd5eeb750030894f33cf1da95f1e2cfd03ad4a8
Guy Lafleur <kevin.netherton@gov.bc.ca>
2023-02-17 10:41:35 -0800

    fix: configure autoincrement to allow dependabots pr to pass (#35)
    * fix: configure autoincrement to allow dependabots pr to pass
    
    * fix: don't need to create envs that go accross steps
    
    * fix: add version increment

--- src/FOMUserUtil/__init__.py
+++ src/FOMUserUtil/__init__.py
@@ -1,3 +1,6 @@
 import os.path
 name = os.path.basename(os.path.dirname(__file__))
-version = '0.0.9'
+# version = '0.0.9' moving version to VERSION file, easier to update
+import importlib.metadata
+__version__ = importlib.metadata.version('FOMUserUtil')
+print(__version__)
